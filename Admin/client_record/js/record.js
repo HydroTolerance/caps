f704f0fa6866d5c187031f98af8342235d54f2f4
HydroTolerance <julzhafiz@gmal.com>
2023-08-25 22:10:27 +0800

    Add this so i make many changes

---
+++ Admin/client_record/js/record.js
@@ -0,0 +1,58 @@
+function handleInsertResponse(success) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+    if (success) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Success!',
+            text: 'Data Updated successfully.'
+        }).then(function() {
+            window.location.href = '.edit_client_record.php?id=' + id;
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Failed to add data.'
+        });
+    }
+})
+};
+
+
+
+
+/* window.addEventListener('DOMContentLoaded', (event) => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Success!',
+        text: 'Data Updated successfully.'
+    }).then(function() {
+        window.location.href = 'edit_client_record.php?id=" . $id . "';
+    });
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'Failed to add data.'
+    });
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Success!',
+        text: 'Data Updated successfully.'
+    }).then(function() {
+        window.location.href = 'client_record.php';
+    });
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'Failed to add data.'
+    });
+}); */
